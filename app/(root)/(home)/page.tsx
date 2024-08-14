44573a6485c8cfb458be7ab513b88c544c54b12d
ngoclong <ngoclong2663@gmail.com>
2024-08-14 22:37:53 +0700

    chore: setup metadata & font family

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <UserButton />
       <h1 className="h1-bold">Home</h1>
     </>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,34 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: [
+    "300", // 300 - Light
+    "400", // 400 - Regular
+    "500", // 500 - Medium
+    "600", // 600 - SemiBold
+    "700", // 700 - Bold
+    "800", // 800 - ExtraBold
+    "900", // 900 - Black
+  ],
+  variable: "--font-inter"
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk"
+});
 
 export const metadata: Metadata = {
   title: "Dev Stack Over Flow",
   description: "This is a stack over flow clone",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -13,9 +37,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500"
+        }
+      }}
+    >
       <html suppressContentEditableWarning lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
       </html>
     </ClerkProvider>
   );
